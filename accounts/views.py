ffcfbad273f7ef7e30f5ed9252c09dd7553846d5
Ashish Gupta <ashishkrgupta.cse@gmail.com>
2024-07-11 17:26:03 +0530

    added message alerts

--- accounts/views.py
+++ accounts/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render,redirect
-
+from django.contrib import messages
 # Create your views here.
 def register(request):
     if request.method == 'POST':
        #REGISTER USER
-       return
+       messages.error(request ,'Testing error message' )
+       return redirect('register')
     return render(request,'accounts/register.html')
 
 def login(request):
     if request.method == 'POST':
        #LOGIN USER
-       return
+       messages.error(request ,'Testing error message' )
+       return redirect('login')
     return render(request,'accounts/login.html')
 
 def dashboard(request):
